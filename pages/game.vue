8e4e2b7cd729619b970b0c8c22f352655010837a
Brad Davies <varbrad@users.noreply.github.com>
2018-01-18 13:15:54 +0000

    Show consent form

---
+++ pages/game.vue
@@ -0,0 +1,102 @@
+<template>
+  <section class="container">
+    <div class="canvas">
+      <canvas ref="canvas"></canvas>
+    </div>
+    <div class="controls">
+      <button>Reset</button>
+      <nuxt-link to="/">Back</nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script>
+import Game from '@varbrad/hoai-game'
+
+export default {
+  data() {
+    return {
+      game: null
+    }
+  },
+  mounted() {
+    this.game = new Game(this.$refs.canvas, 640, 480)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/vars';
+
+@keyframes pop-in {
+  from {
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+  }
+}
+
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: $dark;
+
+  > .canvas {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    > canvas {
+      animation: pop-in 500ms ease-out;
+      border: 4px solid $primary;
+      box-shadow: 8px 8px 0 rgba(0, 0, 0, 0.2);
+      background: white;
+    }
+  }
+
+  > .controls {
+    width: 100%;
+    background: rgba(0, 0, 0, 0.1);
+    padding: 1rem;
+    display: flex;
+    align-items: stretch;
+    justify-content: center;
+
+    button,
+    a {
+      transition: background 150ms ease-out, color 150ms ease-out,
+        border-color 150ms ease-out;
+      cursor: pointer;
+      background: $dark;
+      color: rgba($primary, 0.2);
+      border: 3px solid rgba($primary, 0.2);
+      font-weight: bold;
+      padding: 0.25rem 0.5rem;
+      border-radius: 3px;
+      text-decoration: none;
+      outline: none;
+      font-size: 1rem;
+      margin: 0 0.5rem;
+    }
+
+    &:hover {
+      button,
+      a {
+        border-color: $primary;
+        color: $primary;
+        &:hover {
+          color: $dark;
+          background: $primary;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,143 @@
 <template>
   <section class="container">
-    <canvas ref="canvas"></canvas>
+    <div class="box">
+      <h1>Activity Consent</h1>
+      <div class="grid-a">
+          <div class="row">
+            <p>1</p>
+            <span>I have received and read the provided Participant Information Sheet.</span>
+          </div>
+          <div class="row">
+            <p>2</p>
+            <span>The activity has been fully explained, and I understand what my participation will involve.</span>
+          </div>
+          <div class="row">
+            <p>3</p>
+            <span>I understand that my participation is entirely voluntary, and that I am free to withdraw at any time, without giving reason.</span>
+          </div>
+          <div class="row">
+            <p>4</p>
+            <span>I understand how the data I provide will be used and stored in the research process.</span>
+          </div>
+        </div>
+        <div class="grid-b">
+          <h2>By clicking 'I Agree to Participate', you are confirming that the above requirements have been met.</h2>
+          <nuxt-link to="/game" exact>I Agree to Participate</nuxt-link>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-import Game from '@varbrad/hoai-game'
-
-export default {
-  data() {
-    return {
-      game: null
-    }
-  },
-  mounted() {
-    this.game = new Game(this.$refs.canvas, 640, 480)
-  }
-}
 </script>
 
-<style lang="scss">
-$dark: #314256;
-$primary: #f1e202;
+<style lang="scss" scoped>
+@import '../assets/vars';
+
+@keyframes pop-in {
+  from {
+    transform: translateY(-10%);
+    opacity: 0.5;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
 
 .container {
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
   background-color: $dark;
+}
 
-  > canvas {
-    border: 4px solid $primary;
-    box-shadow: 8px 8px 0 rgba(0, 0, 0, 0.2);
-    background: white;
+.box {
+  animation: pop-in 500ms ease-out 1;
+  max-width: 900px;
+  border: 3px solid $primary;
+  border-radius: 3px;
+  padding: 1rem;
+
+  > h1 {
+    margin: -1rem -1rem 1rem -1rem;
+    padding: 1rem;
+    font-size: 2rem;
+    background: $primary;
+  }
+
+  > .grid-a {
+    border-radius: 3px;
+    overflow: hidden;
+
+    > .row {
+      display: flex;
+      align-items: center;
+      background-color: white;
+      padding: 1rem 0;
+
+      > p {
+        width: 8rem;
+        text-align: center;
+        font-size: 2rem;
+        color: $secondary;
+        font-weight: bold;
+      }
+
+      > span {
+        flex: 1;
+        text-align: left;
+      }
+
+      &:nth-child(odd) {
+        background-color: darken(white, 5%);
+      }
+    }
+  }
+
+  > .grid-b {
+    border-radius: 3px;
+    overflow: hidden;
+    background-color: $secondary;
+    border: 3px solid $primary;
+    margin-top: 1rem;
+    padding: 1rem;
+
+    display: flex;
+    align-items: center;
+
+    > h2 {
+      flex: 1;
+      color: white;
+      font-size: 1rem;
+      font-weight: 400;
+      text-align: justify;
+    }
+
+    > a {
+      cursor: pointer;
+      padding: 1rem 2rem;
+      margin-left: 1rem;
+      font-size: 1.2rem;
+      font-weight: bold;
+      background: linear-gradient(lighten($primary, 5%), $primary);
+      border: 4px solid $dark;
+      color: $dark;
+      border-radius: 1rem;
+      text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
+      outline: none;
+      text-decoration: none;
+
+      &:hover {
+        background: white;
+        border-color: $primary;
+      }
+    }
   }
 }
 </style>
